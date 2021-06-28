9c943b6e2c3ee91ada030bacf8fb914e373eeffb
Rodrigo Busata <rodrigo.busata@gmail.com>
2021-06-28 00:06:39 -0300

    Did Scratch

--- SmartPromo/SmartPromo.h
+++ SmartPromo/SmartPromo.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 12/21/20.
 //
-//  1.3.1
+//  1.4.0
 //
 
 #import <UIKit/UIKit.h>
--- SmartPromoCore.xcframework/ios-arm64_i386_x86_64-simulator/SmartPromoCore.framework/Headers/SmartPromoCore.h
+++ SmartPromoCore.xcframework/ios-arm64_i386_x86_64-simulator/SmartPromoCore.framework/Headers/SmartPromoCore.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 05/11/21.
 //
-//  1.3.1
+//  1.4.0
 //
 
 #import <UIKit/UIKit.h>
