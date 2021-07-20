75aa5e30d717002d00b5150eeb368ecde4301728
Rodrigo Busata <rodrigo.busata@gmail.com>
2021-07-20 00:15:36 -0300

    Prize improvements

--- SmartPromo/SmartPromo.h
+++ SmartPromo/SmartPromo.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 12/21/20.
 //
-//  1.4.0
+//  1.4.1
 //
 
 #import <UIKit/UIKit.h>
--- SmartPromoCore.xcframework/ios-arm64_armv7/SmartPromoCore.framework/Headers/SmartPromoCore.h
+++ SmartPromoCore.xcframework/ios-arm64_armv7/SmartPromoCore.framework/Headers/SmartPromoCore.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 05/11/21.
 //
-//  1.4.0
+//  1.4.1
 //
 
 #import <UIKit/UIKit.h>
