6212b94628216af18580c6aaf2d967dddffd816c
Rodrigo Busata <rodrigo.busata@gmail.com>
2021-10-14 01:01:24 -0300

    Version 1.5.1

--- SmartPromoCore.xcframework/ios-arm64_armv7/SmartPromoCore.framework/Headers/SmartPromoCore.h
+++ SmartPromoCore.xcframework/ios-arm64_armv7/SmartPromoCore.framework/Headers/SmartPromoCore.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 05/11/21.
 //
-//  1.5.0
+//  1.5.1
 //
 
 #import <UIKit/UIKit.h>
--- output/SmartPromo-iphonesimulator.xcarchive/Products/Library/Frameworks/SmartPromo.framework/Headers/SmartPromo.h
+++ output/SmartPromo-iphonesimulator.xcarchive/Products/Library/Frameworks/SmartPromo.framework/Headers/SmartPromo.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 12/21/20.
 //
-//  1.4.5
+//  1.5.1
 //
 
 #import <UIKit/UIKit.h>
