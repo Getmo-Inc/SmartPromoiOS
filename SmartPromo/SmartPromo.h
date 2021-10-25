fcb582f6054ee628b126acf2473318c06db91dcb
Rodrigo Busata <rodrigo.busata@gmail.com>
2021-10-24 22:27:07 -0300

    Removed formated values

--- SmartPromo/SmartPromo.h
+++ SmartPromo/SmartPromo.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 12/21/20.
 //
-//  1.5.3
+//  1.5.4
 //
 
 #import <UIKit/UIKit.h>
--- SmartPromoCore.xcframework/ios-arm64_i386_x86_64-simulator/SmartPromoCore.framework/Headers/SmartPromoCore.h
+++ SmartPromoCore.xcframework/ios-arm64_i386_x86_64-simulator/SmartPromoCore.framework/Headers/SmartPromoCore.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 05/11/21.
 //
-//  1.5.3
+//  1.5.4
 //
 
 #import <UIKit/UIKit.h>
