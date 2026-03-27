5b6f86879153e0802b24746dfc3d26c108cc5869
Rodrigo Busata <rodrigo.busata@gmail.com>
2026-03-26 21:10:13 -0400

    Fix switch campaigns

--- SmartPromo/SmartPromo.h
+++ SmartPromo/SmartPromo.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 12/21/20.
 //
-//  3.0.0 (10)
+//  3.0.0 (11)
 //
 
 #import <UIKit/UIKit.h>
--- SmartPromoCore.xcframework/ios-arm64/SmartPromoCore.framework/Headers/SmartPromoCore.h
+++ SmartPromoCore.xcframework/ios-arm64/SmartPromoCore.framework/Headers/SmartPromoCore.h
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Busata on 05/11/21.
 //
-//  3.0.0 (10)
+//  3.0.0 (11)
 //
 
 #import <UIKit/UIKit.h>
